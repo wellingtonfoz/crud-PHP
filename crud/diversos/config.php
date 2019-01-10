4e32c9a8e7e980c34b7dfdf28e59f4c1a43680ed
Wellington de Oliveira <wellington.oliveira.foz@gmail.com>
2019-01-10 21:00:19 -0200

    Update config.php

--- crud/diversos/config.php
+++ crud/diversos/config.php
@@ -5,12 +5,12 @@
 		$mail->SMTPDebug  = 2;          
 		$mail->SMTPAuth   = true;       
 		$mail->SMTPSecure = "tls";      
-	    $mail->Host = 'email-smtp.us-east-1.amazonaws.com:587';
+	    	$mail->Host = 'smtp.gmail.com';
 		$mail->Port       = 587;        
-		$mail->Username = 'AKIAJ3K6UZAREJPOS2CA';
-		$mail->Password = 'Ar0aYP2EWaBW5T4V1kPI6IH5kFNx1U+0zqkA1XN7fzdv';
+		$mail->Username = 'email@email.com.br';
+		$mail->Password = 'SENHAAAAAAAAA';
 		$mail->CharSet = 'UTF-8';			
-	    $mail->From = 'naoresponda@udc.edu.br';
+	    	$mail->From = 'email@email.com.br';
 		$mail->FromName = 'Notificador de Propostas do Site';
 			
 ?>
